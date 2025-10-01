a70840d98bf1be0b8c05818fafe37942232267d8
Fernando <125690001+FMJara@users.noreply.github.com>
2025-10-01 14:07:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, send_from_directory
 import pandas as pd
 import numpy as np
@@ -135,5 +136,5 @@ def serve_data(symbol):
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
